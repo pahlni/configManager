19e035d0202f678aa54e09494900284d08bda5b3
Nicholas Pahl <nicholas.pahl@dornerworks.com>
2017-11-22 13:59:26 +0000

    Added git lg alias

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ ln -sf ~/configManager/.vimrc ~
 
 # install ctags
 sudo apt-get install ctags
+
+# adding "git lg" alias for git log
+sudo apt-get install git -y
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
